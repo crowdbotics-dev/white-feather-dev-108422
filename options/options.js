576ebf01faa631638c5cc8c4be79b19da7985692
Crowdbotics <engineering@crowdbotics.com>
2023-08-11 06:52:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-feather-dev-108422",
+  url: "https://white-feather-dev-108422.botics.co",
+  api: "https://white-feather-dev-108422.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
